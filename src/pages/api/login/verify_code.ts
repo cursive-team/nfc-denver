812d9401ae5b98fe7fed8453c4341257ca1b6114
AndrewCLu <andrewclu98@gmail.com>
2024-01-21 03:53:17 -0500

    fetch backup upon login

--- src/pages/api/login/verify_code.ts
+++ src/pages/api/login/verify_code.ts
@@ -6,10 +6,25 @@ import {
   AuthTokenResponse,
 } from "../_auth";
 import { ErrorResponse } from "../_types";
+import { BackupResponse } from "../backup";
+import { decryptString } from "@/lib/backup";
+
+export type LoginResponse =
+  | {
+      authToken: AuthTokenResponse;
+      backup: BackupResponse;
+      password:
+        | {
+            salt: string;
+            hash: string;
+          }
+        | undefined;
+    }
+  | ErrorResponse;
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<AuthTokenResponse | ErrorResponse>
+  res: NextApiResponse<LoginResponse | ErrorResponse>
 ) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
@@ -32,6 +47,53 @@ export default async function handler(
     return res.status(404).json({ error: "User not found" });
   }
 
+  // Generate auth token
   const authToken = await generateAuthToken(user.id);
-  return res.status(200).json(authToken);
+
+  // Get latest backup
+  // TODO: Might want to perform some logic here ensuring the backup encryption method matches the current user custody preference
+  const backup = await prisma.backup.findFirst({
+    where: { userId: user.id },
+    orderBy: { createdAt: "desc" },
+  });
+  if (!backup) {
+    return res.status(404).json({ error: "Backup not found" });
+  }
+
+  const { encryptedData, authenticationTag, iv } = backup;
+
+  if (backup.isServerEncrypted) {
+    const serverEncryptionEmail = process.env.SERVER_ENCRYPTION_EMAIL!;
+    const serverEncryptionPassword = process.env.SERVER_ENCRYPTION_PASSWORD!;
+    const decryptedBackup = decryptString(
+      encryptedData,
+      authenticationTag,
+      iv,
+      serverEncryptionEmail,
+      serverEncryptionPassword
+    );
+
+    return res.status(200).json({
+      authToken,
+      backup: { decryptedData: decryptedBackup },
+      password: undefined,
+    });
+  } else {
+    if (!user.passwordSalt || !user.passwordHash) {
+      return res.status(404).json({ error: "Password not found" });
+    }
+
+    return res.status(200).json({
+      authToken,
+      backup: {
+        encryptedData,
+        authenticationTag,
+        iv,
+      },
+      password: {
+        salt: user.passwordSalt,
+        hash: user.passwordHash,
+      },
+    });
+  }
 }
