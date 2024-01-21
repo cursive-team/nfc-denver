9184acc658f1f1e6c699dd1afe737a057bd2a7ba
AndrewCLu <andrewclu98@gmail.com>
2024-01-21 03:53:17 -0500

    fetch backup upon login

--- src/pages/api/backup.ts
+++ src/pages/api/backup.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/lib/prisma";
 import { object, string } from "yup";
-import { encryptString } from "@/lib/backup";
+import { decryptString, encryptString } from "@/lib/backup";
 import { verifyAuthToken } from "./_auth";
 import { EmptyResponse, ErrorResponse } from "./_types";
 import { backupSchema } from "@/util/localStorage";
@@ -18,91 +18,145 @@ export const encryptedBackupDataSchema = object({
   iv: string().required(),
 });
 
+export type BackupResponse = EncryptedBackupData | { decryptedData: string };
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<EmptyResponse | ErrorResponse>
+  res: NextApiResponse<BackupResponse | EmptyResponse | ErrorResponse>
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method Not Allowed" });
-  }
+  // TODO: Is this endpoint needed?
+  if (req.method === "GET") {
+    const { authToken } = req.query;
 
-  const { backup, wantsServerCustody, authToken } = req.body;
-
-  // Validate authToken
-  const userId = await verifyAuthToken(authToken);
-  if (userId === undefined) {
-    return res.status(401).json({ error: "Invalid or expired auth token" });
-  }
-
-  // Retrieve user and check wantsServerCustody matches
-  const user = await prisma.user.findUnique({
-    where: { id: userId },
-  });
-  if (!user) {
-    return res.status(404).json({ error: "User not found" });
-  }
-
-  // For safety, check that wantsServerCustody matches
-  if (user.wantsServerCustody !== wantsServerCustody) {
-    return res.status(400).json({ error: "Mismatch in custody preference" });
-  }
-
-  // TODO: Clean up old backups while adding new ones
-
-  if (wantsServerCustody) {
-    // If wantsServerCustody, backup must be a string
-    if (typeof backup !== "string") {
-      return res.status(400).json({ error: "Invalid backup" });
+    if (typeof authToken !== "string") {
+      return res.status(400).json({ error: "Invalid auth token" });
     }
 
-    try {
-      const parsedBackup = JSON.parse(backup);
-      const validatedBackup = backupSchema.validateSync(parsedBackup);
-      if (!validatedBackup) {
-        throw new Error("Invalid backup");
-      }
-    } catch (error) {
-      return res.status(400).json({ error: "Invalid backup" });
+    // Validate authToken
+    const userId = await verifyAuthToken(authToken);
+    if (userId === undefined) {
+      return res.status(401).json({ error: "Invalid or expired auth token" });
     }
-    const serverEncryptionEmail = process.env.SERVER_ENCRYPTION_EMAIL!;
-    const serverEncryptionPassword = process.env.SERVER_ENCRYPTION_PASSWORD!;
-    const { encryptedData, authenticationTag, iv } = encryptString(
-      backup,
-      serverEncryptionEmail,
-      serverEncryptionPassword
-    );
 
-    await prisma.backup.create({
-      data: {
-        userId: user.id,
+    // Retrieve user
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Get latest backup
+    // TODO: Might want to perform some logic here ensuring the backup encryption method matches the current user custody preference
+    const backup = await prisma.backup.findFirst({
+      where: { userId: user.id },
+      orderBy: { createdAt: "desc" },
+    });
+    if (!backup) {
+      return res.status(404).json({ error: "Backup not found" });
+    }
+
+    const { encryptedData, authenticationTag, iv } = backup;
+
+    if (backup.isServerEncrypted) {
+      const serverEncryptionEmail = process.env.SERVER_ENCRYPTION_EMAIL!;
+      const serverEncryptionPassword = process.env.SERVER_ENCRYPTION_PASSWORD!;
+      const decryptedBackup = decryptString(
         encryptedData,
         authenticationTag,
         iv,
-        isServerEncrypted: true,
-      },
+        serverEncryptionEmail,
+        serverEncryptionPassword
+      );
+      return res.status(200).json({ decryptedData: decryptedBackup });
+    }
+
+    return res.status(200).json({
+      encryptedData,
+      authenticationTag,
+      iv,
     });
-  } else {
-    // If !wantsServerCustody, backup must be an object with encryptedData, authenticationTag, and iv
-    try {
-      const backupData = encryptedBackupDataSchema.validateSync(backup);
-      if (!backupData) {
-        throw new Error("Invalid backup");
+  } else if (req.method === "POST") {
+    const { backup, wantsServerCustody, authToken } = req.body;
+
+    // Validate authToken
+    const userId = await verifyAuthToken(authToken);
+    if (userId === undefined) {
+      return res.status(401).json({ error: "Invalid or expired auth token" });
+    }
+
+    // Retrieve user and check wantsServerCustody matches
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // For safety, check that wantsServerCustody matches
+    if (user.wantsServerCustody !== wantsServerCustody) {
+      return res.status(400).json({ error: "Mismatch in custody preference" });
+    }
+
+    // TODO: Clean up old backups while adding new ones
+
+    if (wantsServerCustody) {
+      // If wantsServerCustody, backup must be a string
+      if (typeof backup !== "string") {
+        return res.status(400).json({ error: "Invalid backup" });
       }
 
-      const { encryptedData, authenticationTag, iv } = backupData;
+      try {
+        const parsedBackup = JSON.parse(backup);
+        const validatedBackup = backupSchema.validateSync(parsedBackup);
+        if (!validatedBackup) {
+          throw new Error("Invalid backup");
+        }
+      } catch (error) {
+        return res.status(400).json({ error: "Invalid backup" });
+      }
+      const serverEncryptionEmail = process.env.SERVER_ENCRYPTION_EMAIL!;
+      const serverEncryptionPassword = process.env.SERVER_ENCRYPTION_PASSWORD!;
+      const { encryptedData, authenticationTag, iv } = encryptString(
+        backup,
+        serverEncryptionEmail,
+        serverEncryptionPassword
+      );
+
       await prisma.backup.create({
         data: {
           userId: user.id,
           encryptedData,
           authenticationTag,
           iv,
-          isServerEncrypted: false,
+          isServerEncrypted: true,
         },
       });
-    } catch (error) {
-      return res.status(400).json({ error: "Invalid backup" });
-    }
-  }
+    } else {
+      // If !wantsServerCustody, backup must be an object with encryptedData, authenticationTag, and iv
+      try {
+        const backupData = encryptedBackupDataSchema.validateSync(backup);
+        if (!backupData) {
+          throw new Error("Invalid backup");
+        }
 
-  return res.status(200).json({});
+        const { encryptedData, authenticationTag, iv } = backupData;
+        await prisma.backup.create({
+          data: {
+            userId: user.id,
+            encryptedData,
+            authenticationTag,
+            iv,
+            isServerEncrypted: false,
+          },
+        });
+      } catch (error) {
+        return res.status(400).json({ error: "Invalid backup" });
+      }
+    }
+
+    return res.status(200).json({});
+  } else {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { saveAuthToken, getProfile, Profile } from "../util/localStorage";
+import {
+  saveAuthToken,
+  getProfile,
+  Profile,
+  loadBackup,
+} from "../util/localStorage";
+import { encryptedBackupDataSchema } from "./api/backup";
+import { hashPassword } from "@/lib/password";
+import { decryptString } from "@/lib/backup";
 
 enum DisplayState {
   INPUT_EMAIL,
   INPUT_CODE,
+  INPUT_PASSWORD,
 }
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [code, setCode] = useState("");
+  const [password, setPassword] = useState("");
   const [displayState, setDisplayState] = useState(DisplayState.INPUT_EMAIL);
+  const [encryptedData, setEncryptedData] = useState("");
+  const [authenticationTag, setAuthenticationTag] = useState("");
+  const [iv, setIv] = useState("");
+  const [passwordSalt, setPasswordSalt] = useState("");
+  const [passwordHash, setPasswordHash] = useState("");
   const router = useRouter();
 
   const handleEmailSubmit = async (event: React.FormEvent) => {
@@ -47,8 +62,82 @@ export default function Login() {
       return;
     }
 
-    // Todo: fetch profile and keys from backup and save to local storage. validate login response
-    router.push("/");
+    // Todo: validate login response
+
+    // Password hint is provided if user chooses self custody
+    if (data.password) {
+      try {
+        const { encryptedData, authenticationTag, iv } =
+          encryptedBackupDataSchema.validateSync(data.backupData);
+
+        // Save auth token
+        const { value, expiresAt } = data.authToken;
+        saveAuthToken(value, new Date(expiresAt));
+
+        // User must confirm password to decrypt data
+        setEncryptedData(encryptedData);
+        setAuthenticationTag(authenticationTag);
+        setIv(iv);
+        setPasswordSalt(data.password.salt);
+        setPasswordHash(data.password.hash);
+
+        setDisplayState(DisplayState.INPUT_PASSWORD);
+      } catch (error) {
+        console.error(error);
+        alert("Error logging in. Please try again.");
+        return;
+      }
+    } else {
+      if (
+        !data.backupData ||
+        !data.backupData.decryptedData ||
+        typeof data.backupData.decryptedData !== "string"
+      ) {
+        alert("Error logging in. Please try again.");
+        return;
+      }
+
+      try {
+        loadBackup(data.backupData.decryptedData);
+      } catch (error) {
+        console.error(error);
+        alert("Error logging in. Please try again.");
+        return;
+      }
+
+      // Save auth token
+      const { value, expiresAt } = data.authToken;
+      saveAuthToken(value, new Date(expiresAt));
+
+      router.push("/");
+    }
+  };
+
+  const handlePasswordSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const derivedPasswordHash = await hashPassword(password, passwordSalt);
+    if (derivedPasswordHash !== passwordHash) {
+      alert("Incorrect password!");
+      return;
+    }
+
+    try {
+      const decryptedBackupData = decryptString(
+        encryptedData,
+        authenticationTag,
+        iv,
+        email,
+        password
+      );
+      loadBackup(decryptedBackupData);
+
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+      alert("Error logging in. Please try again.");
+      return;
+    }
   };
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -57,6 +146,10 @@ export default function Login() {
 
   const handleCodeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCode(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
   };
 
   return (
@@ -113,6 +206,38 @@ export default function Login() {
                 name="code"
                 value={code}
                 onChange={handleCodeChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
+                required
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        )}
+        {displayState === DisplayState.INPUT_PASSWORD && (
+          <form
+            onSubmit={handlePasswordSubmit}
+            className="bg-white dark:bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 dark:text-gray-200 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={handlePasswordChange}
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
                 required
               />
