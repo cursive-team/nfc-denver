85d4553ebc18985ca4d67a0acc46b93c450501e9
Vivek Bhupatiraju <vivekab@mit.edu>
2024-02-19 23:04:48 -0700

    fix quest requirement finishing display

--- src/components/modals/StoreItemModal.tsx
+++ src/components/modals/StoreItemModal.tsx
@@ -4,12 +4,19 @@ import { ListLayout } from "@/layouts/ListLayout";
 import { useRedeemStoreItem } from "@/hooks/useStore";
 import Link from "next/link";
 import { QuestCard } from "../cards/QuestCard";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Button } from "../Button";
-import { getAllQuestCompleted } from "@/lib/client/localStorage";
+import {
+  LocationSignature,
+  User,
+  getAllQuestCompleted,
+  getLocationSignatures,
+  getUsers,
+} from "@/lib/client/localStorage";
 import { ItemWithCompletion, QuestWithRequirements } from "@/types";
 import toast from "react-hot-toast";
 import { classed } from "@tw-classed/react";
+import { computeNumRequirementsSatisfied } from "@/lib/client/quests";
 
 const QRCodeWrapper = classed.div(
   "bg-white rounded-[8px] w-full max-w-[156px]"
@@ -28,6 +35,38 @@ const StoreModalItem = ({
   const questCompleted = getAllQuestCompleted();
   const completedQuestIds: string[] = Object.keys(questCompleted);
   const [qrCodeUrl, setQrCodeUrl] = useState<string>();
+
+  // Compute users and locations that user has signatures for
+  const [userPublicKeys, setUserPublicKeys] = useState<string[]>([]);
+  const [locationPublicKeys, setLocationPublicKeys] = useState<string[]>([]);
+
+  useEffect(() => {
+    const users = getUsers();
+    const locationSignatures = getLocationSignatures();
+
+    const validUserPublicKeys = Object.values(users)
+      .filter((user: User) => user.sig)
+      .map((user: User) => user.sigPk!);
+    setUserPublicKeys(validUserPublicKeys);
+
+    const validLocationPublicKeys = Object.values(locationSignatures).map(
+      (location: LocationSignature) => location.pk
+    );
+    setLocationPublicKeys(validLocationPublicKeys);
+  }, []);
+
+  const numRequirementsSatisfied: number[] = useMemo(() => {
+    return storeItem.questRequirements.map(
+      ({ userRequirements, locationRequirements }: QuestWithRequirements) => {
+        return computeNumRequirementsSatisfied({
+          userPublicKeys,
+          locationPublicKeys,
+          userRequirements,
+          locationRequirements,
+        });
+      }
+    );
+  }, [storeItem.questRequirements, userPublicKeys, locationPublicKeys]);
 
   const redeemStoreItemMutation = useRedeemStoreItem();
   const onRedeemItem = async () => {
@@ -87,30 +126,37 @@ const StoreModalItem = ({
             </Button>
           )}
         </div>
-        <ListLayout label="Complete the following quests to redeem">
-          {questRequirements.map(
-            ({
-              id,
-              name,
-              description,
-              userRequirements,
-              locationRequirements,
-            }: QuestWithRequirements) => {
-              return (
-                <Link href={`/quests/${id}`} key={id}>
-                  <QuestCard
-                    title={name}
-                    description={description}
-                    completedSigs={1}
-                    userRequirements={userRequirements}
-                    locationRequirements={locationRequirements}
-                    isCompleted={completedQuestIds.includes(id.toString())}
-                  />
-                </Link>
-              );
-            }
-          )}
-        </ListLayout>
+        {questRequirements.length !== 0 && (
+          <ListLayout label="Complete the following quests to redeem">
+            <div className="flex flex-col gap-2">
+              {questRequirements.map(
+                (
+                  {
+                    id,
+                    name,
+                    description,
+                    userRequirements,
+                    locationRequirements,
+                  }: QuestWithRequirements,
+                  index: number
+                ) => {
+                  return (
+                    <Link href={`/quests/${id}`} key={id}>
+                      <QuestCard
+                        title={name}
+                        description={description}
+                        completedSigs={numRequirementsSatisfied[index]}
+                        userRequirements={userRequirements}
+                        locationRequirements={locationRequirements}
+                        isCompleted={completedQuestIds.includes(id.toString())}
+                      />
+                    </Link>
+                  );
+                }
+              )}
+            </div>
+          </ListLayout>
+        )}
       </div>
     </Modal>
   );
