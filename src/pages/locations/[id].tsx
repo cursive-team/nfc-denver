1f5cb9505fe382e8bc5881298d295ae5eb24abca
Kalidou Diagne <kld.diagne@gmail.com>
2024-02-22 16:57:37 +0100

    location tap view + refactoring

--- src/pages/locations/[id].tsx
+++ src/pages/locations/[id].tsx
@@ -1,29 +1,47 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { ErrorResponse } from "@/types";
+import { use, useEffect, useState } from "react";
+import { ErrorResponse, QuestWithCompletion } from "@/types";
 import { Location } from "@prisma/client";
 import {
   LocationSignature,
   getLocationSignature,
 } from "@/lib/client/localStorage";
-import { classed } from "@tw-classed/react";
-import { Header } from "@/components/modals/QuestRequirementModal";
-import useSettings from "@/hooks/useSettings";
 import { AppBackHeader } from "@/components/AppHeader";
 import { toast } from "sonner";
 import { LoadingWrapper } from "@/components/wrappers/LoadingWrapper";
 import { LocationDetailPlaceholder } from "@/components/placeholders/LocationDetailPlaceholder";
-import { getNonceFromCounterMessage } from "@/lib/client/libhalo";
-
-const Label = classed.span("text-xs text-gray-10 font-light");
-const Description = classed.span("text-gray-12 text-sm font-light");
+import Image from "next/image";
+import { ListLayout } from "@/layouts/ListLayout";
+import { useFetchQuests } from "@/hooks/useFetchQuests";
+import { QuestCard } from "@/components/cards/QuestCard";
+import { useQuestRequirements } from "@/hooks/useQuestRequirements";
+import { Card } from "@/components/cards/Card";
+import { Placeholder } from "@/components/placeholders/Placeholder";
+import { set } from "react-hook-form";
 
 const LocationDetails = () => {
-  const { pageWidth } = useSettings();
   const router = useRouter();
   const { id } = router.query;
   const [location, setLocation] = useState<Location>();
   const [signature, setSignature] = useState<LocationSignature>();
+  const [relatedQuests, setRelatedQuests] = useState<QuestWithCompletion[]>([]);
+
+  const { isPending: isLoadingQuests, data = [] } = useFetchQuests();
+
+  // TODO: get related quests should be replaced with a server call
+  useEffect(() => {
+    if (!location) return;
+    const relatedQuests = data.filter((quest) => {
+      const locationRequirements = quest.locationRequirements.filter(
+        (requirement) =>
+          requirement.locations.find((loc) => +loc.id === location.id)
+      );
+
+      return locationRequirements.length > 0;
+    });
+
+    setRelatedQuests(relatedQuests);
+  }, [data, location]);
 
   useEffect(() => {
     const fetchLocation = async () => {
@@ -50,11 +68,13 @@ const LocationDetails = () => {
     };
 
     fetchLocation();
-  }, [router, id]);
+  }, [router, id, isLoadingQuests]);
+
+  const { numRequirementsSatisfied } = useQuestRequirements(relatedQuests);
 
   return (
     <div>
-      <AppBackHeader redirectTo="/" />
+      <AppBackHeader redirectTo="/quests" />
       <LoadingWrapper
         isLoading={!location}
         fallback={<LocationDetailPlaceholder />}
@@ -62,38 +82,68 @@ const LocationDetails = () => {
       >
         {location && (
           <>
-            <Header title={location.name} label="Location" />
-            <div className="flex flex-col gap-4">
-              <div
-                className="flex bg-slate-200 rounded bg-center bg-cover"
-                style={{
-                  width: `${pageWidth - 32}px`,
-                  height: `${pageWidth - 32}px`,
-                  backgroundImage: `url(${location.imageUrl})`,
-                }}
-              />
-              <div className="flex flex-col gap-4 jus">
-                <div className="flex flex-col">
-                  <Label>Description</Label>
-                  <Description>{location.description}</Description>
+            <div className="flex flex-col items-center gap-[10px] pt-24 pb-28">
+              <div className="flex bg-[#677363] justify-center items-center h-10 w-10 rounded-full ">
+                <Image
+                  src="/icons/home.png"
+                  height={16}
+                  width={16}
+                  alt="home image"
+                />
+              </div>
+              <div className="flex flex-col gap-[10px] items-center mx-6">
+                <span className=" text-xl tracking-[-0.2px] font-light text-gray-12">
+                  Success!
+                </span>
+                <div className="flex gap-0.5 text-xs font-light">
+                  <span className=" text-gray-12">{`${location.name}`}</span>
+                  <span className=" text-gray-11">has been completed</span>
                 </div>
-                {signature !== undefined && (
-                  <div className="flex flex-col">
-                    <Label>Visited On</Label>
-                    <Description>{`${signature.ts}`}</Description>
-                  </div>
+                {location?.description?.length > 0 && (
+                  <span className="text-gray-11 text-center">
+                    {location.description}
+                  </span>
                 )}
-                {signature !== undefined &&
-                  getNonceFromCounterMessage(signature.msg) !== undefined && (
-                    <div className="flex flex-col">
-                      <Label>Visitor No.</Label>
-                      <Description>{`${getNonceFromCounterMessage(
-                        signature.msg
-                      )}`}</Description>
-                    </div>
-                  )}
               </div>
             </div>
+            <ListLayout label="Continue quest">
+              <LoadingWrapper
+                className="flex flex-col gap-2"
+                isLoading={isLoadingQuests}
+                fallback={
+                  <>
+                    <Placeholder.List items={2} />
+                  </>
+                }
+              >
+                {relatedQuests?.map(
+                  (
+                    {
+                      id,
+                      name,
+                      description,
+                      userRequirements,
+                      locationRequirements,
+                      isCompleted = false,
+                    },
+                    index
+                  ) => {
+                    if (isCompleted) return null; // no need to show completed quests
+                    return (
+                      <QuestCard
+                        key={id}
+                        title={name}
+                        description={description}
+                        completedSigs={numRequirementsSatisfied[index]}
+                        userRequirements={userRequirements}
+                        locationRequirements={locationRequirements}
+                        isCompleted={isCompleted}
+                      />
+                    );
+                  }
+                )}
+              </LoadingWrapper>
+            </ListLayout>
           </>
         )}
       </LoadingWrapper>
