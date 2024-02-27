6666fe4c057744c915295be88ae67fbce4a059a8
Vivek Bhupatiraju <vivekab@mit.edu>
2024-02-27 16:37:26 -0700

    art code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -38,6 +39,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script defer src="/bundle.js"></Script>
       </body>
     </Html>
   );
