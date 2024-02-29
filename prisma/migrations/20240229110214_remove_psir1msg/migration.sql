85bedd245e5fe26a566e69df4651cd8054efe7be
Vivek Bhupatiraju <vivekab@mit.edu>
2024-02-29 04:02:34 -0700

    remove psir1msg column too

---
+++ prisma/migrations/20240229110214_remove_psir1msg/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `psiRound1Message` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "psiRound1Message";
